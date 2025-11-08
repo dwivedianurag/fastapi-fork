db4ab3459f9a48c4e9b307d3368ca0943021027a
dwivedianurag <anurag.dwivedi@macrometa.com>
2025-11-09 02:05:38 +0530

    something

--- fastapi/cli.py
+++ fastapi/cli.py
@@ -4,8 +4,14 @@ try:
 except ImportError:  # pragma: no cover
     cli_main = None  # type: ignore
 
+sample = {
+      "name": "FastAPI",
+      "version": 1,
+      "features": ["literals", "aliases"],
+  }
 
 def main() -> None:
+    message = 'To use the fastapi command, please install "fastapi[standard]":\n\n\tpip install "fastapi[standard]"\n'
     if not cli_main:  # type: ignore[truthy-function]
         message = 'To use the fastapi command, please install "fastapi[standard]":\n\n\tpip install "fastapi[standard]"\n'
         print(message)
